446d7e580972ee5bac373e1fec34d343bca5238a
i4ni <mikevidotto@live.com>
2026-09-15 09:40:38 -0400

    fixed typo for 10 foot studs.

--- internal/list/list.go
+++ internal/list/list.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CreateListFile(data materialcalculator.LumberData) error {
-    body := fmt.Sprintf("Studs\n\n2x4x8: %d\n2x4x19: %d\n2x4x12: %d\n2x4x16: %d\n\nSheets\n\n3/4\": %d\n1/2\": %d\nSmartSide: %d\n", data.Eight, data.Ten, data.Twelve, data.Sixteen, data.Floorply, data.Roofply, data.Finishedply)
+    body := fmt.Sprintf("Studs\n\n2x4x8: %d\n2x4x10: %d\n2x4x12: %d\n2x4x16: %d\n\nSheets\n\n3/4\": %d\n1/2\": %d\nSmartSide: %d\n", data.Eight, data.Ten, data.Twelve, data.Sixteen, data.Floorply, data.Roofply, data.Finishedply)
     fmt.Println(body)
     name := "list.txt"
 
